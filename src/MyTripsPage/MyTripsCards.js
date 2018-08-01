bbb4a92eb556cbb7c92b0031634a26283816ea79
leahgwin <leahgwin@gmail.com>
2018-08-01 15:17:09 -0500

    add css to the cards so they're easy to read

--- src/MyTripsPage/MyTripsCards.js
+++ src/MyTripsPage/MyTripsCards.js
@@ -4,22 +4,25 @@ import PropTypes from "prop-types"
 import { Jumbotron, Button } from "react-bootstrap";
 import ApiManager from "../API/ApiManager";
 import { Component } from "react";
+import "./MyTripsCards.css"
 
 
 export default class MyTripsCards extends Component {
 
     render() {
         return (
-            < div >
-                <Jumbotron>
-                    <h1>{this.props.mytrip.name}</h1>
-                    <p>{this.props.mytrip.startDate}</p>
-                    <p>{this.props.mytrip.endDate}</p>
+            <div className="card" style={{ width: `75rem` }}>
+                <div className="card-body">
+                    <h2 className="card-title">{this.props.mytrip.name}</h2>
+                    <h5 className="card-title">Start Date of Trip:</h5>
+                    <p className="card-text">{this.props.mytrip.startDate}</p>
+                    <h5 className="card-title">End Date of Trip:</h5>
+                    <p className="card-text">{this.props.mytrip.endDate}</p>
 
                     <p>
                         <Button bsStyle="primary">Vote and View Itinerary</Button>
                     </p>
-                </Jumbotron>
+                </div>
             </div >
         )
 
