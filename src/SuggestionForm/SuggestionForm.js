c83ad8782fb3bd039da5259ab20eac447587a287
leahgwin <leahgwin@gmail.com>
2018-08-01 15:27:46 -0500

    starting to work on the form for submissions

--- src/SuggestionForm/SuggestionForm.js
+++ src/SuggestionForm/SuggestionForm.js
@@ -1,11 +1,100 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import ApiManager from "../API/ApiManager";
 
 export default class SuggestionForm extends Component {
+    constructor(props) {
+        super(props);
+        //this is where I'm storing the input that the user inputs into the suggestion form
+        this.state = {
+            name: "",
+            cost: "",
+            description: "",
+            link: ""
+        };
+    }
+    //this is just making sure the field isn't empty. I can also add further requirements if I want to
+    validateForm() {
+        return this.state.name.length > 0 && this.state.startDate.length > 0 && this.state.endDate.length > 0;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        let signedInUser = JSON.parse(localStorage.getItem("credentials"));
+        if (signedInUser === null) {
+            signedInUser = JSON.parse(sessionStorage.getItem("credentials"));
+            signedInUser = signedInUser.user;
+        } else {
+            signedInUser = signedInUser.user;
+        }
+        console.log("signed user", signedInUser);
+
+
+        let newGroup = {
+            name: this.state.name,
+            startDate: this.state.startDate,
+            endDate: this.state.endDate,
+            userId: ApiManager.getIdofCurrentUser
+        }
+
+        ApiManager.postItem("groups", newGroup)
+            .then(() => {
+                //redirects to "my trips" so they can see what they added
+                this.props.history.push('/mytrips')
+            })
+    };
+
+
+
     render() {
         return (
-
-            <React.Fragment><div><h1>SUGGESTIONS</h1> </div></React.Fragment>
-        )
+            <div className="groupTrip">
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="name" bsSize="large">
+                        <ControlLabel>Name of Trip (Group + Location)</ControlLabel>
+                        <FormControl
+                            //autofocus gives attn to the email input for the user to know where to type
+                            autoFocus
+                            type="text"
+                            placeholder="(i.e. Smith Family- London)"
+                            //this sets the state each time when user inputs
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="startDate" bsSize="large">
+                        <ControlLabel>Start Date of Trip</ControlLabel>
+                        <FormControl
+                            value={this.state.startDate}
+                            onChange={this.handleChange}
+                            type="date"
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="endDate" bsSize="large">
+                        <ControlLabel>End Date of Trip</ControlLabel>
+                        <FormControl
+                            value={this.state.endDate}
+                            onChange={this.handleChange}
+                            type="date"
+                        />
+                    </FormGroup>
+                    <Button
+                        block
+                        bsSize="large"
+                        disabled={!this.validateForm()}
+                        type="submit"
+                    >
+                        Add Group Trip
+          </Button>
+                </form>
+            </div>
+        );
     }
 }
