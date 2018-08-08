82ad49dc0a313838384c69b9907b4b6dfed85748
leahgwin <leahgwin@gmail.com>
2018-08-08 14:12:49 -0500

    fix the suggestion trip & the my trip from duplicating

--- src/MyTripsPage/MyTripsList.js
+++ src/MyTripsPage/MyTripsList.js
@@ -21,22 +21,31 @@ export default class MyTripsList extends Component {
             .then(groups => {
                 console.log("groups", groups);
                 this.setState({ groups: groups })
+            }).then((suggestions) => {
+
+                ApiManager.getUserSuggestionTrip(userId)
+                    .then(allTrips => {
+                        allTrips.forEach(trip => {
+                            console.log("trip", trip);
+
+                            if (trip.userId !== userId) {
+                                // console.log("alltrips", allTrips.userId);
+                                console.log("userId", userId);
+
+                                let remDup = this.removeDuplicates(allTrips, "groupId")
+                                console.log("all trips", allTrips);
+
+                                // console.log("suggestions", suggestions);
+                                this.setState({ suggestions: remDup })
+                            }
+                        })
+
+                    })
+
             })
 
-        // this is the previous call i was making that was giving me duplicates of suggestions
-        // ApiManager.getUserSuggestionTrip(userId)
-        //     .then(allTrips => {
-        //         console.log(allTrips);
 
-        //         this.setState({ suggestions: allTrips })
-        //     })
 
-        ApiManager.getUserSuggestionTrip(userId)
-            .then(allTrips => {
-                let remDup = this.removeDuplicates(allTrips, "groupId")
-                // console.log("suggestions", suggestions);
-                this.setState({ suggestions: remDup })
-            })
     }
     //here's where I got this code: https://ilikekillnerds.com/2016/05/removing-duplicate-objects-array-property-name-javascript/
     removeDuplicates = (myArr, prop) => {
