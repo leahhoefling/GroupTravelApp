490c29927a310351be9315aa0897d72e3ca8cff9
leahgwin <leahgwin@gmail.com>
2018-08-09 13:42:22 -0500

    MVP for real now

--- src/MyTripsPage/MyTripsList.js
+++ src/MyTripsPage/MyTripsList.js
@@ -46,35 +46,6 @@ export default class MyTripsList extends Component {
             return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
         });
     }
-    //>>>>>>>>>>this code wasnt working so refactored to above code with the promise.all>>>>>>>>>>
-    // ApiManager.getUserTrip("groups", userId)
-    //     .then(groups => {
-    //         console.log("groups", groups);
-    //         this.setState({ groups: groups })
-    //     })
-    //     .then((suggestions => {
-    //         console.log("suggestions after .then", suggestions);// suggestions is undefined
-    //         ApiManager.getUserSuggestionTrip(userId)
-    //             .then(allTrips => {
-    //                 allTrips.forEach(trip => {
-    //                     // console.log("trip", trip);
-
-    //                     if (trip.userId !== userId) {
-    //                         // console.log("alltrips user id", allTrips.userId);
-    //                         console.log("userId", userId);
-
-    //                         let remDup = this.removeDuplicates(allTrips, "groupId")
-    //                         // console.log("all trips", allTrips);
-
-    //                         console.log("suggestions in foreach", suggestions);//console not getting to this
-    //                         this.setState({ suggestions: remDup })
-    //                     }
-    //                 })
-
-    //             })
-
-    //     }))
-
 
     render() {
         return (
